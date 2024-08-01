3dc2eb0191d8dc5e0b2e903e175ba08f9b36e3cb
spmtripathi10@gmail.com <spmtripathi10@gmail.com>
2024-08-01 15:20:33 +0530

    Working Admin

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'saurav tripathi',
-        'title': 'Blog Post 1 ',
-        'content': 'First post in django',
-        'date_posted': 'August 27 90392'
-    },
-    {
-        'author': 'saurav tripathi',
-        'title': 'Post 2  ',
-        'content': 'Second test post',
-        'date_posted': 'July 12 1832'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
