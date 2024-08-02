3546e163bc79f6a36a7da33fedddeaad6d05ed7f
spmtripathi10@gmail.com <spmtripathi10@gmail.com>
2024-08-02 15:00:49 +0530

    complete profile functionality

--- blog_website/users/forms.py
+++ blog_website/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']  # corrected from 'field' to 'fields'
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']  # corrected from 'field' to 'fields'
